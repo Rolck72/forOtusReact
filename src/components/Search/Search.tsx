957fb771ce7aaa837023a6bcb31ec46af65f3b44
Yura Zolotaev <Rolck73@yandex.ru>
2024-01-29 01:07:21 +0500

    начальная часть

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,21 +1,17 @@
-import { forwardRef } from "react"
-import styles from './Search.module.css'
+import { forwardRef } from 'react';
+import styles from './Search.module.css';
 import cn from 'classnames';
-import { SearchProps } from "./Search.props"
-
-
+import { SearchProps } from './Search.props';
 
 const Search = forwardRef<HTMLInputElement, SearchProps>(function Input({ isValid = true, className, ...props }, ref) {
-    return(
-			<div className={styles['input-wrapper']}>
-					<input ref={ref} className={cn(styles['search'], className, {
+	return (
+		<div className={styles['input-wrapper']}>
+			<input ref={ref} className={cn(styles['input'], className, {
 				[styles['invalid']]: isValid
 			})} {...props} />
-				<img className={styles['icon']} src="./search-icon.svg" alt="Иконка лупы" />
-			</div>
-		
-		)
+			<img className={styles['icon']} src='/search-icon.svg' alt='Иконка лупы' />
+		</div>
+	);
+});
 
-})
-
-export default Search
+export default Search;
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import Headling from "../../components/Headling/Headling";
+import ProductCard from "../../components/ProductCart/ProductCart";
 import Search from "../../components/Search/Search";
 import styles from './Menu.module.css'
 
@@ -8,7 +9,17 @@ export function Menu() {
     <>
       <div className={styles['head']}>
         <Headling>Меню</Headling>
-        <Search />
+        <Search placeholder="введите товар" />
+      </div>
+      <div>
+          <ProductCard
+            id={1}
+            name = 'Наслажднение'
+            description="Салями, руккола"
+            image = '/product-demo.png'
+            rating={4.5}
+            price={300}
+           />
       </div>
     </>
   )
