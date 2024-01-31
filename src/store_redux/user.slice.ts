f27ba7931df72d063001ea9330cadda8db38666d
Yura Zolotaev <Rolck73@yandex.ru>
2024-01-31 22:39:34 +0500

    redux#1

--- src/store_redux/user.slice.ts
+++ src/store_redux/user.slice.ts
@@ -1,11 +1,15 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { loadState } from "./storage";
+
+
+export const JWT_PRESISTENT_STATE = 'jwt'
 
 export interface UserState{
 	jwt: string | null
 }
 
 const initialState: UserState ={
-	jwt: null
+	jwt: loadState(JWT_PRESISTENT_STATE) ?? null
 }
 
 export const userSlice = createSlice({
